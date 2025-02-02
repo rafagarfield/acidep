4b04395b03b16ecce9fbdb588150343398225e35
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-02-02 11:32:14 -0500

    Add ProductForm and ProductList components to the main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Categories from "@/components/Categories/Categories"
+import ProductForm from "@/components/Products/ProductForm"
+import ProductList from "@/components/Products/ProductList"
 
 
 function page() {
   return (
     <div>
       <Categories/>
+      <ProductForm />
+      <ProductList />
     </div>
   )
 }
