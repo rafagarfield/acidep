4b1972e7c2282033d1d06c6207e6c107cf2fb829
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-02-06 12:05:30 -0500

    Add product details page component to display product information and features

---
+++ src/app/products/[productId]/page.jsx
@@ -0,0 +1,17 @@
+import FeaturesBanner from '@/components/FeaturesBanner/FeaturesBanner'
+import ProductDetails from '@/components/Products/ProductDetails/ProductDetails'
+import React from 'react'
+
+function page({params}) {
+    if(!params.productId){
+        return 
+    }
+  return (
+    <div>
+      <ProductDetails productId={params.productId}/>
+      <FeaturesBanner />
+    </div>
+  )
+}
+
+export default page
