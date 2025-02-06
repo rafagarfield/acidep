b9cd1f94d6617c2d1c6fcdbd93e28a43e3a782b5
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-02-06 12:05:35 -0500

    Add shop page component to display shop products and features banner

---
+++ src/app/shops/page.jsx
@@ -0,0 +1,12 @@
+import FeaturesBanner from '@/components/FeaturesBanner/FeaturesBanner'
+import ShopPage from '@/components/Products/ShopProducts/ShopProducts'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <ShopPage />
+      <FeaturesBanner />
+    </div>
+  )
+}
