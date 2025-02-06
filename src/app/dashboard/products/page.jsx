5d622e03d8a01c3dbf90d6bf62a495a0c282f240
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-02-06 12:05:17 -0500

    Add ProductForm component to products page for product addition

---
+++ src/app/dashboard/products/page.jsx
@@ -0,0 +1,12 @@
+import ProductForm from '@/components/Products/ProductForm'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <ProductForm />
+    </div>
+  )
+}
+
+export default page
