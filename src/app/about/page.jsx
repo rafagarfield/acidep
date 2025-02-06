d54781ee28b1fa738a040dd4b1518bb01faeda3c
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-02-06 12:04:43 -0500

    Add About page component for informational content

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+import About from '@/components/About/About'
+import Hero from '@/components/About/Hero'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        {/* <Hero /> */}
+      <About />
+    </div>
+  )
+}
+
+export default page
