97716d2c83270f55b53efefc70d1ea541bfaadad
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-02-06 12:05:13 -0500

    Add ListProducts page component to dashboard for product listing

---
+++ src/app/dashboard/list-products/page.jsx
@@ -0,0 +1,12 @@
+import ProductList from '@/components/Products/ProductList'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <ProductList />
+    </div>
+  )
+}
+
+export default page
