6a2a68631bbbeb0461f44e6007050a876c757ec7
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-02-06 12:05:08 -0500

    Add Categories page component to dashboard for category management

---
+++ src/app/dashboard/categories/page.jsx
@@ -0,0 +1,10 @@
+import Categories from '@/components/Categories/Categories'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <Categories />
+    </div>
+  )
+}
