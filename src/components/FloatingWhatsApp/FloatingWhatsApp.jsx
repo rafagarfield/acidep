92a89e0aaf1a4792064533b65efd17ebca4203df
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-02-16 23:46:12 -0500

    Update WhatsApp link in FloatingWhatsApp component

--- src/components/FloatingWhatsApp/FloatingWhatsApp.jsx
+++ src/components/FloatingWhatsApp/FloatingWhatsApp.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const FloatingWhatsApp = () => {
   return (
     <a
-      href="https://wa.me/917669029" // Reemplaza con tu número de WhatsApp
+      href="https://wa.me/929497479" // Reemplaza con tu número de WhatsApp
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition duration-300 flex items-center justify-center text-3xl"
