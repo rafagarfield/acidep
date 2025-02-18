4307426d8494492235b97311ead58c2416bc70bf
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-02-18 13:33:25 -0500

    Add page component to display ListImage with CarouselManager

---
+++ src/app/dashboard/list-image/page.jsx
@@ -0,0 +1,12 @@
+import CarouselManager from '@/components/ListImage/ListImage'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <CarouselManager/>
+    </div>
+  )
+}
+
+export default page
