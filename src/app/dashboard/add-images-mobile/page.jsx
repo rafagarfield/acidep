2bc5a5160e1c80584e6c5e78f8031641e7cea521
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-02-18 13:33:20 -0500

    Add CarouselFormMobile component to add-images-mobile page

---
+++ src/app/dashboard/add-images-mobile/page.jsx
@@ -0,0 +1,12 @@
+import CarouselFormMobile from '@/components/CarouselFormMobile/CarouselFormMobile'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <CarouselFormMobile/>
+    </div>
+  )
+}
+
+export default page
