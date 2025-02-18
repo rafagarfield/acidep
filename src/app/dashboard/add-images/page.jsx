3ee5de0ce572b6db8c1771f3b396b758a568d3a5
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-02-18 13:33:16 -0500

    Add CarouselForm component to dashboard image upload page

---
+++ src/app/dashboard/add-images/page.jsx
@@ -0,0 +1,10 @@
+import CarouselForm from '@/components/CarouselForm/CarouselForm'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <CarouselForm/>
+    </div>
+  )
+}
