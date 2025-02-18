47453d0d3cb11770ccef16407f41be080121d9e1
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-02-18 13:33:30 -0500

    Add ListImageMobile page component with CarouselManagerMobile

---
+++ src/app/dashboard/list-image-mobile/page.jsx
@@ -0,0 +1,12 @@
+import CarouselManagerMobile from '@/components/ListImageMobile/ListImageMobile'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <CarouselManagerMobile/>
+    </div>
+  )
+}
+
+export default page
