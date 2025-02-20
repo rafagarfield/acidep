2f37c39cb5d16d15fa8c872687a773100930fb4f
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-02-20 16:36:13 -0500

    Wrap list-image-mobile page in ProtectedRoute for access control

--- src/app/dashboard/list-image-mobile/page.jsx
+++ src/app/dashboard/list-image-mobile/page.jsx
@@ -1,11 +1,12 @@
 import CarouselManagerMobile from '@/components/ListImageMobile/ListImageMobile'
+import ProtectedRoute from '@/components/ProtectedRoute/ProtectedRoute'
 import React from 'react'
 
 function page() {
   return (
-    <div>
+    <ProtectedRoute>
       <CarouselManagerMobile/>
-    </div>
+    </ProtectedRoute>
   )
 }
 
