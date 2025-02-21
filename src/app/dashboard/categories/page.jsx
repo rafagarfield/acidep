bb0d70b8465c76d5856cf043293d86c811f088a0
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-02-21 12:44:10 -0500

    Replace ProtectedRoute with ProtectDynamicRouteClient in dashboard categories page

--- src/app/dashboard/categories/page.jsx
+++ src/app/dashboard/categories/page.jsx
@@ -1,11 +1,12 @@
 import Categories from '@/components/Categories/Categories'
-import ProtectedRoute from '@/components/ProtectedRoute/ProtectedRoute'
+import ProtectDynamicRouteClient from '@/components/ProtectDynamicRouteClient/ProtectDynamicRouteClient'
+import ProtectedRoute, { ProtectedRouteDynamic } from '@/components/ProtectedRoute/ProtectedRoute'
 import React from 'react'
 
 export default function page() {
   return (
-    <ProtectedRoute>
+    <ProtectDynamicRouteClient>
       <Categories />
-    </ProtectedRoute>
+    </ProtectDynamicRouteClient>
   )
 }
