e6128dada2c056325d57aa57fb48c5b919eee067
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-02-21 12:44:46 -0500

    Refactor ProtectedRoute component to improve readability and maintainability

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,9 +1,12 @@
 "use client"
 import { redirect } from 'next/navigation'
-import React from 'react'
 
-export default function ProtectedRoute({children}) {
+function ProtectedRoute({children}) {
   return (
     localStorage.getItem('user') ? children : redirect("/login")
   )
 }
+
+export default ProtectedRoute
+
+
