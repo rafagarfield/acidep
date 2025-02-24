86667f7fbb2937b499001a2d63f75fa248c53c65
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-02-23 19:51:16 -0500

    Add list-job page with JobImageList component

---
+++ src/app/dashboard/list-job/page.jsx
@@ -0,0 +1,10 @@
+import JobImageList from '@/components/JobImageList/JobImageList'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <JobImageList/>
+    </div>
+  )
+}
